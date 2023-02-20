0ee7148e5f8962e8a389ba4ab3bdc3e565c63620
Jaron <JHSpaz988@gmail.com>
2023-02-20 13:40:49 -0500

    #100daysofcodingpythonchallenge day1

---
+++ Python_#100daysofcodingchallenge/variables.py
@@ -0,0 +1,13 @@
+# Use variables to store data
+
+name = input("What is your name?") # Ask user for name to insert into variable
+
+print(name) # print() variable to console
+
+name = "Jaron" # Change variable
+
+print(name) # print() new variable to console
+
+length = len(name) # Get length of variable name
+
+print(length) # print length variable
