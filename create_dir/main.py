48f0cda259153f6e2ff5975d4d7b76a3fb9fdc1f
Jaron Haberstroh <JHSpaz988@gmail.com>
2023-05-20 02:17:10 -0400

    Changes to create_dir main.py file to correct error

--- create_dir/main.py
+++ create_dir/main.py
@@ -1,21 +1,22 @@
 import os
+import sys
 
-CWD = "/home/jhspaz988/Documents/python/#100daysofcodingpythonchallenge"
+CWD = "/home/jhspaz988/Documents/python/100daysofcodingpythonchallenge"
 CHILD_LIST = ["projects", "playground"]
 
 os.chdir(CWD)
 
 
-def create_dir(day):
-    dir_name = f"day{day}"
-    try:
-        os.mkdir(dir_name)
-    except FileExistsError:
-        print("file exists")
-    else:
-        for child in CHILD_LIST:
-            os.mkdir(os.path.join(dir_name, child))
+def create_dir(days):
+    for day in days:
+        dir_name = f"day{day}"
+        try:
+            os.mkdir(dir_name)
+        except FileExistsError:
+            print("file exists")
+        else:
+            for child in CHILD_LIST:
+                os.mkdir(os.path.join(dir_name, child))
 
 
-for day in range(1, 100 + 1):
-    create_dir(day)
+create_dir(sys.argv[1:])
