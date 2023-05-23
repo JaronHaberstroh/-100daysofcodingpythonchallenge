b347e758b48c633829fa6214a9be9b0b82fb8d0e
Jaron Haberstroh <JHSpaz988@gmail.com>
2023-05-22 21:15:53 -0400

    Doing some side work with os

--- create_dir/main.py
+++ create_dir/main.py
@@ -1,11 +1,10 @@
 import os
 import sys
 
-CWD = "/home/jhspaz988/Documents/python/100daysofcodingpythonchallenge"
+ROOT = "/home/jhspaz988/Documents/python/100daysofcodingpythonchallenge"
 CHILD_LIST = ["projects", "playground"]
 
-os.chdir(CWD)
-
+os.chdir(ROOT)
 
 def create_dir(days):
     for day in days:
@@ -17,6 +16,8 @@ def create_dir(days):
         else:
             for child in CHILD_LIST:
                 os.mkdir(os.path.join(dir_name, child))
-
+            with open(f"{dir_name}/.gitignore", "x") as file:
+                file.write("playground/config.py\nplayground/__pycache__\nprojects/config.py\nprojects/__pycache__")
+            
 
 create_dir(sys.argv[1:])
---
+++ python_playground/os_path.py
@@ -0,0 +1,24 @@
+import os
+
+# Clear Terminal
+os.system('clear')
+
+# Find current working directory or CWD
+CWD = os.getcwd()
+print(CWD)
+
+# Find absolute path of current working directory or CWD 
+ROOT_DIR = os.path.dirname(CWD)
+print(ROOT_DIR)
+
+# Find absolute path of file
+ABS_PATH = os.path.abspath(CWD)
+print(ABS_PATH)
+
+# Find relative path of file
+REL_PATH = os.path.relpath(CWD)
+print(REL_PATH)
+
+# Change current working directory or CWD
+os.chdir(ABS_PATH)
+print(os.getcwd())
